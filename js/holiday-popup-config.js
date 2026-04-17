7fc9d26f275d87fab481c8ccb949ea08d13aadc7
tawandaeng charnyouth <thos000150@Tawan.local>
2026-04-17 12:28:57 +0700

    Disable homepage popup

--- js/holiday-popup-config.js
+++ js/holiday-popup-config.js
@@ -1,5 +1,5 @@
 window.HOLIDAY_POPUP_CONFIG = Object.freeze({
-    enabled: true,
+    enabled: false,
     campaignId: 'songkran-69',
     imageSrc: 'assets/images/popups/songkran-69-popup.webp',
     imageAlt: 'Songkran Festival popup for MediaKids Academy',
